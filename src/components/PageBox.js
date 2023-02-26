64b367c7a00b2d72249effa1f371dd20bf067fa4
Aidan Scudder <aidan.scudder@gmail.com>
2023-02-25 22:21:57 -0700

    style refactor

--- src/components/PageBox.js
+++ src/components/PageBox.js
@@ -1,7 +1,7 @@
-import '../styles/PageBox.css';
 import React, { useState } from 'react';
 import { AddPageButton } from './AddPageButton';
 import { levelColors } from '../styles/levelColors';
+import '../styles/PageBox.css'
 
 export const PageBox = ({ name, id, level, handleDelete }) => {
 	const [subPages, setSubPages] = useState([]);
@@ -12,85 +12,64 @@ export const PageBox = ({ name, id, level, handleDelete }) => {
 		setSubPages(updatedPages);
 	};
 
+	const pageBoxStyle = {
+		margin: level === 0 ? '10px' : 0,
+		width: 'auto',
+		minWidth: level > 0 ? '90%' : '100px',
+		alignSelf: level > 0 ? 'flex-end' : 'flex-start',
+		backgroundColor: levelColors[level] ?? 'white',
+		boxShadow: '0 5px 10px rgba(0, 0, 0, 0.5)',
+		borderRadius: '8px',
+		marginBottom: '10px',
+		marginLeft: level > 0 ? '20px' : '10px',
+		color: level >= 4 ? 'black' : 'white',
+		position: 'relative',
+	};
+
+	const pageNameStyle = {
+		padding: '10px',
+		fontSize: `${3 - level / 3}rem`,
+		fontWeight: 'bold',
+		marginBottom: '10px',
+		alignSelf: level === 0 ? 'center' : 'flex-start',
+		textTransform: 'uppercase',
+		letterSpacing: '1px',
+		whiteSpace: 'nowrap',
+		overflow: 'hidden',
+		textOverflow: 'ellipsis',
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	};
+
 	return (
 		<div
 			id={id}
-			style={{
-				margin: level === 0 ? '10px' : 0,
-				width: 'auto',
-				minWidth: level > 0 ? '90%' : '100px',
-				alignSelf: level > 0 ? 'flex-end' : 'flex-start',
-				backgroundColor: levelColors[level] ?? 'white',
-				boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-				borderRadius: '8px',
-				marginBottom: '10px',
-				marginLeft: level > 0 ? '20px' : '10px',
-				color: level >= 4 ? 'black' : 'white',
-				position: 'relative',
-			}}
+			style={pageBoxStyle}
 			onMouseEnter={() => setShowDeleteIcon(true)}
 			onMouseLeave={() => setShowDeleteIcon(false)}
-			className="pageBoxContainer"
 		>
-			<div
-				style={{
-					padding: '10px',
-					fontSize: `${3 - level / 2}rem`,
-					fontWeight: 'bold',
-					marginBottom: '10px',
-					alignSelf: level === 0 ? 'center' : 'flex-start',
-					textTransform: 'uppercase',
-					letterSpacing: '1px',
-
-					whiteSpace: 'nowrap',
-					overflow: 'hidden',
-					textOverflow: 'ellipsis', // add this to shorten long names with an ellipsis,
-					display: 'flex',
-					flexDirection: 'row',
-					alignItems: 'center',
-					justifyContent: 'space-around',
-				}}
-			>
-				{name}
+			<div style={pageNameStyle}>
+				<span>{name}</span>
 				{showDeleteIcon && (
-					<div
-						style={{
-							position: 'absolute',
-							top: '-20px',
-							right: '-20px',
-							cursor: 'pointer',
-							backgroundColor: 'red',
-							width: '40px',
-							height: '40px',
-							color: 'white',
-							fontSize: '20px',
-							borderRadius: '20px',
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-						}}
-						onClick={() => handleDelete(name)}
-					>
+					<div className={'deleteIcon'} onClick={() => handleDelete(name)}>
 						X
 					</div>
 				)}
 				<AddPageButton
-					onAddPage={(newSubPage) => {
-						setSubPages([...subPages, newSubPage]);
-					}}
+					onAddPage={(newSubPage) => setSubPages([...subPages, newSubPage])}
 				/>
 			</div>
-			{subPages.map((page, index) => {
-				return (
-					<PageBox
-						level={level + 1}
-						name={page}
-						id={page.id}
-						key={`${page}-${index}`}
-						handleDelete={handleDeleteSubpage}
-					/>
-				);
-			})}
+			{subPages.map((page, index) => (
+				<PageBox
+					level={level + 1}
+					name={page}
+					id={`${id}-${index}`}
+					key={`${id}-${index}`}
+					handleDelete={handleDeleteSubpage}
+				/>
+			))}
 		</div>
 	);
 };
