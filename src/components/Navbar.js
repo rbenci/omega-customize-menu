d93cf99ed97f24f987ca0882691228a09c563940
Aidan Scudder <aidan.scudder@gmail.com>
2023-02-25 22:02:19 -0700

    closes Create functionality to delete a page #10

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,16 @@ import { PageBox } from './PageBox';
 export const Navbar = () => {
 	const [mainPages, setMainPages] = useState(['Page 1', 'Page 2', 'Page 3']);
 
+  const deleteMainPage = (name) => {
+		const updatedPages = mainPages.filter((page) => page !== name);
+		setMainPages(updatedPages);
+	};
+
 	return (
 		<div className="navbarContainer">
 			<div className="horizontalPageList">
 				{mainPages.map((page, index) => {
-					return <PageBox key={`${page}-${index}`} name={page} level={0} />;
+					return <PageBox key={`${page}-${index}`} name={page} level={0} handleDelete={deleteMainPage} />;
 				})}
 			</div>
 			<AddPageButton
--- src/components/PageBox.js
+++ src/components/PageBox.js
@@ -3,38 +3,81 @@ import React, { useState } from 'react';
 import { AddPageButton } from './AddPageButton';
 import { levelColors } from '../styles/levelColors';
 
-export const PageBox = ({ name, id, level }) => {
+export const PageBox = ({ name, id, level, handleDelete }) => {
 	const [subPages, setSubPages] = useState([]);
+	const [showDeleteIcon, setShowDeleteIcon] = useState(false);
+
+	const handleDeleteSubpage = (name) => {
+		const updatedPages = subPages.filter((page) => page !== name);
+		setSubPages(updatedPages);
+	};
 
 	return (
 		<div
 			id={id}
 			style={{
 				margin: level === 0 ? '10px' : 0,
-				padding: '1rem',
-				width: '90%',
-				justifySelf: level > 0 ? 'flex-end' : 'center',
+				width: 'auto',
+				minWidth: level > 0 ? '90%' : '100px',
+				alignSelf: level > 0 ? 'flex-end' : 'flex-start',
 				backgroundColor: levelColors[level] ?? 'white',
 				boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
 				borderRadius: '8px',
 				marginBottom: '10px',
 				marginLeft: level > 0 ? '20px' : '10px',
-				color: 'white',
+				color: level >= 4 ? 'black' : 'white',
+				position: 'relative',
 			}}
+			onMouseEnter={() => setShowDeleteIcon(true)}
+			onMouseLeave={() => setShowDeleteIcon(false)}
 			className="pageBoxContainer"
 		>
 			<div
 				style={{
+					padding: '10px',
 					fontSize: `${3 - level / 2}rem`,
 					fontWeight: 'bold',
 					marginBottom: '10px',
+					alignSelf: level === 0 ? 'center' : 'flex-start',
 					textTransform: 'uppercase',
 					letterSpacing: '1px',
-					color: level > 0 ? 'white' : 'black',
-                    whiteSpace:'nowrap'
+					
+					whiteSpace: 'nowrap',
+					overflow: 'hidden',
+					textOverflow: 'ellipsis', // add this to shorten long names with an ellipsis,
+					display: 'flex',
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-around',
 				}}
 			>
 				{name}
+				{showDeleteIcon && (
+					<div
+						style={{
+							position: 'absolute',
+							top: '-20px',
+							right: '-20px',
+							cursor: 'pointer',
+							backgroundColor: 'red',
+							width: '40px',
+							height: '40px',
+							fontSize: '20px',
+							borderRadius: '20px',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+						onClick={() => handleDelete(name)}
+					>
+						X
+					</div>
+				)}
+				<AddPageButton
+					onAddPage={(newSubPage) => {
+						setSubPages([...subPages, newSubPage]);
+					}}
+				/>
 			</div>
 			{subPages.map((page, index) => {
 				return (
@@ -43,14 +86,10 @@ export const PageBox = ({ name, id, level }) => {
 						name={page}
 						id={page.id}
 						key={`${page}-${index}`}
+						handleDelete={handleDeleteSubpage}
 					/>
 				);
 			})}
-			<AddPageButton
-				onAddPage={(newSubPage) => {
-					setSubPages([...subPages, newSubPage]);
-				}}
-			/>
 		</div>
 	);
 };
