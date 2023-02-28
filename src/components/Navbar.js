d05fb0e542d70f2521af9c0b9e8630c19c912385
Paul Lu <sininglu@sandiego.edu>
2023-02-27 17:27:03 -0800

    Create a React component to represent the entire navigation structure #3 Finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,63 @@
 /** @format */
-import '../styles/Navbar.css';
-import React, { useState } from 'react';
+import "../styles/Navbar.css";
+import React, { createContext, useState } from "react";
 
-import { AddPageButton } from './AddPageButton';
-import { PageBox } from './PageBox';
+import { AddPageButton } from "./AddPageButton";
+import { PageBox } from "./PageBox";
+
+export const PageContext = createContext();
 
 export const Navbar = () => {
-	const [mainPages, setMainPages] = useState(['Page 1', 'Page 2', 'Page 3']);
+  const [mainPages, setMainPages] = useState(["Page 1", "Page 2", "Page 3"]);
+  const [pageStructure, setPageStructure] = useState({
+    "Page 1": [],
+    "Page 2": [],
+    "Page 3": [],
+  });
 
   const deleteMainPage = (name) => {
-		const updatedPages = mainPages.filter((page) => page !== name);
-		setMainPages(updatedPages);
-	};
+    const updatedPages = mainPages.filter((page) => page !== name);
+    setMainPages(updatedPages);
 
-	return (
-		<div className="navbarContainer">
-			<div className="horizontalPageList">
-				{mainPages.map((page, index) => {
-					return <PageBox key={`${page}-${index}`} name={page} level={0} handleDelete={deleteMainPage} />;
-				})}
-			</div>
-			<AddPageButton
-				onAddPage={(newPage) => {
-					setMainPages([...mainPages, newPage]);
-				}}
-			/>
-		</div>
-	);
+    const newPageStructure = { ...pageStructure };
+    delete newPageStructure[name];
+
+    setPageStructure(newPageStructure);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(pageStructure);
+  };
+
+  return (
+    <PageContext.Provider value={[pageStructure, setPageStructure]}>
+      <div className="navbarContainer">
+        <div className="horizontalPageList">
+          {mainPages.map((page, index) => {
+            return (
+              <PageBox
+                key={`${page}-${index}`}
+                id={`${page}-${index}`}
+                name={page}
+                level={0}
+                handleDelete={deleteMainPage}
+              />
+            );
+          })}
+        </div>
+        <AddPageButton
+          onAddPage={(newPage) => {
+            setMainPages([...mainPages, newPage]);
+
+            const newPageStructure = { ...pageStructure };
+            newPageStructure[newPage] = [];
+            setPageStructure(newPageStructure);
+          }}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    </PageContext.Provider>
+  );
 };
--- src/components/PageBox.js
+++ src/components/PageBox.js
@@ -1,75 +1,96 @@
-import React, { useState } from 'react';
-import { AddPageButton } from './AddPageButton';
-import { levelColors } from '../styles/levelColors';
-import '../styles/PageBox.css'
+import React, { useContext, useState } from "react";
+import { AddPageButton } from "./AddPageButton";
+import { levelColors } from "../styles/levelColors";
+import "../styles/PageBox.css";
+import { PageContext } from "./Navbar";
 
 export const PageBox = ({ name, id, level, handleDelete }) => {
-	const [subPages, setSubPages] = useState([]);
-	const [showDeleteIcon, setShowDeleteIcon] = useState(false);
+  const [subPages, setSubPages] = useState([]);
+  const [showDeleteIcon, setShowDeleteIcon] = useState(false);
+  const [pageStructure, setPageStructure] = useContext(PageContext);
 
-	const handleDeleteSubpage = (name) => {
-		const updatedPages = subPages.filter((page) => page !== name);
-		setSubPages(updatedPages);
-	};
+  const handleDeleteSubpage = (item2Del) => {
+    const updatedPages = subPages.filter((page) => page !== item2Del);
+    setSubPages(updatedPages);
 
-	const pageBoxStyle = {
-		margin: level === 0 ? '10px' : 0,
-		width: 'auto',
-		minWidth: level > 0 ? '90%' : '100px',
-		alignSelf: level > 0 ? 'flex-end' : 'flex-start',
-		backgroundColor: levelColors[level] ?? 'white',
-		boxShadow: '0 5px 10px rgba(0, 0, 0, 0.5)',
-		borderRadius: '8px',
-		marginBottom: '10px',
-		marginLeft: level > 0 ? '20px' : '10px',
-		color: level >= 4 ? 'black' : 'white',
-		position: 'relative',
-	};
+	const newPageStructure = {...pageStructure}
+	delete newPageStructure[item2Del]
 
-	const pageNameStyle = {
-		padding: '10px',
-		fontSize: `${3 - level / 3}rem`,
-		fontWeight: 'bold',
-		marginBottom: '10px',
-		alignSelf: level === 0 ? 'center' : 'flex-start',
-		textTransform: 'uppercase',
-		letterSpacing: '1px',
-		whiteSpace: 'nowrap',
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-	};
+	const newParentSub = newPageStructure[name]?.filter(e => e !== item2Del)
+	newPageStructure[name] = [newParentSub]; 
+	
+	setPageStructure(newPageStructure)
 
-	return (
-		<div
-			id={id}
-			style={pageBoxStyle}
-			onMouseEnter={() => setShowDeleteIcon(true)}
-			onMouseLeave={() => setShowDeleteIcon(false)}
-		>
-			<div style={pageNameStyle}>
-				<span>{name}</span>
-				{showDeleteIcon && (
-					<div className={'deleteIcon'} onClick={() => handleDelete(name)}>
-						X
-					</div>
-				)}
-				<AddPageButton
-					onAddPage={(newSubPage) => setSubPages([...subPages, newSubPage])}
-				/>
-			</div>
-			{subPages.map((page, index) => (
-				<PageBox
-					level={level + 1}
-					name={page}
-					id={`${id}-${index}`}
-					key={`${id}-${index}`}
-					handleDelete={handleDeleteSubpage}
-				/>
-			))}
-		</div>
-	);
+	// console.log(pageStructure);
+  };
+
+
+  const pageBoxStyle = {
+    margin: level === 0 ? "10px" : 0,
+    width: "auto",
+    minWidth: level > 0 ? "90%" : "100px",
+    alignSelf: level > 0 ? "flex-end" : "flex-start",
+    backgroundColor: levelColors[level] ?? "white",
+    boxShadow: "0 5px 10px rgba(0, 0, 0, 0.5)",
+    borderRadius: "8px",
+    marginBottom: "10px",
+    marginLeft: level > 0 ? "20px" : "10px",
+    color: level >= 4 ? "black" : "white",
+    position: "relative",
+  };
+
+  const pageNameStyle = {
+    padding: "10px",
+    fontSize: `${3 - level / 3}rem`,
+    fontWeight: "bold",
+    marginBottom: "10px",
+    alignSelf: level === 0 ? "center" : "flex-start",
+    textTransform: "uppercase",
+    letterSpacing: "1px",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  };
+
+  return (
+    <div
+      id={id}
+      style={pageBoxStyle}
+      onMouseEnter={() => setShowDeleteIcon(true)}
+      onMouseLeave={() => setShowDeleteIcon(false)}
+    >
+      <div style={pageNameStyle}>
+        <span>{name}</span>
+        {showDeleteIcon && (
+          <div className={"deleteIcon"} onClick={() => handleDelete(name)}>
+            X
+          </div>
+        )}
+        <AddPageButton
+          onAddPage={(newSubPage) => {
+            setSubPages([...subPages, newSubPage]);
+
+            const newPageStructure = { ...pageStructure };
+			newPageStructure[name] = [...newPageStructure[name], newSubPage]
+            newPageStructure[newSubPage] = []
+			setPageStructure(newPageStructure);
+			
+          }}
+        />
+      </div>
+      {subPages.map((page, index) => (
+        <PageBox
+          level={level + 1}
+          name={page}
+          id={`${id}-${index}`}
+          key={`${id}-${index}`}
+          handleDelete={handleDeleteSubpage}
+        />
+      ))}
+    </div>
+  );
 };
