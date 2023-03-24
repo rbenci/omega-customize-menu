71d9b850474f60bfda4f44e7bed888e89d4c2d4d
Paul Lu <paul.l.sining@gmail.com>
2023-03-24 13:16:15 -0700

    added email/company input to popup

--- src/components/PopupWindow.jsx
+++ src/components/PopupWindow.jsx
@@ -13,10 +13,25 @@ export default function PopupWindow({
 }) {
   const form = React.useRef();
 
+  const [userEmail, setUserEmail] = React.useState("");
+  const [userCompany, setUserCompany] = React.useState("");
+
   // console.log(html);
   const handleSendEmail = (e) => {
     e.preventDefault();
     console.log("form", form.current);
+
+
+    if (!userEmail.trim()) {
+      toast.error("Please enter your email");
+      return;
+    }
+
+    if (!userCompany.trim()) {
+      toast.error("Please enter your company");
+      return;
+    }
+
     sendEmail(form.current)
       .then((res) => {
         if (res.status === 200) {
@@ -50,14 +65,42 @@ export default function PopupWindow({
 
           <form ref={form} onSubmit={handleSendEmail}>
             <input type="hidden" name="message" value={html} />
-            <div className="btn btn-secondary" onClick={handleCancel}>
-              Cancel
+            <div className="input-group mb-3">
+              <label htmlFor="email" className="input-group-text">
+                Email
+              </label>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+                placeholder="Enter your email"
+              />
+
+              <label htmlFor="userCompany" className="input-group-text">
+                Company
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="company"
+                value={userCompany}
+                onChange={(e) => setUserCompany(e.target.value)}
+                placeholder="Enter your company"
+              />
             </div>
-            <input
-              type="submit"
-              className="btn btn-primary  ms-3"
-              value="Send"
-            />
+
+            <div className="popupButtons">
+              <div className="btn btn-secondary" onClick={handleCancel}>
+                Cancel
+              </div>
+              <input
+                type="submit"
+                className="btn btn-primary  ms-3"
+                value="Send"
+              />
+            </div>
           </form>
         </div>
       </Popup>
--- src/components/buttons/SubmitBtn.jsx
+++ src/components/buttons/SubmitBtn.jsx
@@ -1,11 +1,18 @@
 /** @format */
 
 import React from 'react';
+import { toast } from 'react-toastify';
 
 export default function SubmitBtn({ list, setShowPopup, createHirarchy }) {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		// console.log(list);
+
+		if(list.length === 0) {
+			toast.error('Please add a page to your menu first');
+			return;
+		}
+
 		setShowPopup((prev) => !prev);
 		createHirarchy(list);
 	};
@@ -16,6 +23,7 @@ export default function SubmitBtn({ list, setShowPopup, createHirarchy }) {
 				type="button"
 				className="btn btn-dark important"
 				onClick={handleSubmit}
+
 			>
 				Submit
 			</button>
